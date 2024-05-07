5285cb97fef31a9d535aabb16c9a5d5377f164ac
tyaku <c0b2315233@edu.teu.ac.jp>
2024-05-07 14:37:51 +0900

    練習２ 爆弾を打ち落とせる機能の追加

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -157,16 +157,23 @@ def main():
                 beam = Beam(bird)
         screen.blit(bg_img, [0, 0])
         
-        if bird.rct.colliderect(bomb.rct):
-            # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
-            bird.change_img(8, screen)
-            pg.display.update()
-            time.sleep(1)
-            return
+        if beam is not None and bomb is not None:   #bomb,beamインスタンスが存在する場合 
+            if beam.rct.colliderect(bomb.rct):      #爆弾とビームが当たった場合
+                bomb = None                         #ビームとボムを消す
+                beam = None
+                 
+        if bomb is not None:
+            if bird.rct.colliderect(bomb.rct):
+                # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
+                bird.change_img(8, screen)
+                pg.display.update()
+                time.sleep(1)
+                return
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
-        bomb.update(screen)
+        if bomb is not None:
+            bomb.update(screen)
         if beam is not None:
             beam.update(screen)
         pg.display.update()
