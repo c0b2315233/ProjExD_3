26755364d3bc396024486bb89236779dfd211740
tyaku <c0b2315233@edu.teu.ac.jp>
2024-05-07 15:13:49 +0900

    練習4　ゲームオーバー画面を追加

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -170,6 +170,11 @@ def main():
                 bird.change_img(8, screen)
                 pg.display.update()
                 time.sleep(1)
+                fonto = pg.font.Font(None, 80) 
+                txt = fonto.render("Game Over", True, (255, 0, 0)) 
+                screen.blit(txt, [WIDTH/2-150, HEIGHT/2]) 
+                pg.display.update() 
+                time.sleep(5) 
                 return
 
         key_lst = pg.key.get_pressed()
